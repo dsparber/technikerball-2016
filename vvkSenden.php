30d4e80014bd99f3965055a6cdfbf9af49aec50d
Daniel Sparber <sparber.daniel@gmail.com>
2015-12-09 15:17:57 +0100

    corrected title

--- vvkSenden.php
+++ vvkSenden.php
@@ -33,7 +33,7 @@
 <html>
 
 <head>
-	<title>Technikerball</title>
+	<title>Technikerball 2016</title>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
